bbcfc6c9bbef38fd15d9d7c86358322d990b24ae
ghazyayman <muhghazyayman@gmail.com>
2022-12-11 22:28:59 +0800

    fix error

---
+++ app/src/main/java/com/example/todolisttutorial/TaskViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.todolisttutorial
+
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+
+class TaskViewModel(private val repository: TaskItemRepository): ViewModel()
+{
+    val taskItems: LiveData<List<TaskItem>> = repository.allTaskItems.asLiveData()
+
+    fun addTaskItem(taskItem: TaskItem) = viewModelScope.launch {
+        repository.insertTaskItem(taskItem)
+    }
+
+    fun updateTaskItem(taskItem: TaskItem) = viewModelScope.launch {
+        repository.updateTaskItem(taskItem)
+    }
+
+    fun setCompleted(taskItem: TaskItem) = viewModelScope.launch {
+        if (!taskItem.isCompleted())
+            taskItem.completedDateString = TaskItem.dateFormatter.format(LocalDate.now())
+        repository.updateTaskItem(taskItem)
+    }
+}
+
+class TaskItemModelFactory(private val repository: TaskItemRepository) : ViewModelProvider.Factory
+{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T
+    {
+        if (modelClass.isAssignableFrom(TaskViewModel::class.java))
+            return TaskViewModel(repository) as T
+
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
